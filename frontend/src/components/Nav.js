72c9eff2721a748f80d64bcd0b9fbb106605b531
Hanna Ylä-Jussila <hanna.yla-jussila@myy.haaga-helia.fi>
2019-10-30 06:12:39 +0200

    nav not working

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,6 +1,26 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
 
 const Nav = ({ changeView }) => {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link onClick={ changeView('sleepperiods') }>
+            Home 
+          </Nav.Link>
+          <Nav.Link onClick={ changeView('settings') }>
+              Settings
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+}
+
+
+/*const Nav = ({ changeView }) => {
   return (
     <nav id='nav'>
       <ul>
@@ -13,6 +33,6 @@ const Nav = ({ changeView }) => {
       </ul>
     </nav>
   )
-}
+}*/
 
 export default Nav
