86a8a3f29198bc28c9446640011de24a332f9c84
Hanna Ylä-Jussila <hanna.yla-jussila@myy.haaga-helia.fi>
2019-10-31 19:56:49 +0200

    date and type broken

--- frontend/src/components/ExternalForm.js
+++ frontend/src/components/ExternalForm.js
@@ -5,15 +5,16 @@ import Button from 'react-bootstrap/Button';
 const ExternalForm = ({ inputs, select, submit }) => {
     return (
         <Form onSubmit={submit}>
-            <Form.Group controlId="formExternal"></Form.Group>
+            <Form.Group controlId="formExternal">
             {inputs.map(i => <div key={i.name}>
                 <Form.Label column sm={2}>{i.name}: </Form.Label>
                 <input type={i.type} value={i.value} onChange={i.onChange} ></input></div>)}
+            </Form.Group>
             <Form.Group controlId="formExternalType">
                 <Form.Label>Type: </Form.Label>
-                <Form.Control as="select" value={select.value} onChange={select.onChange}>
-                    {select.values.map(v => <option key={v.id} value={v.value}>{v.value}</option>)}
-                </Form.Control>
+                    <Form.Control as="select" value={select.value} onChange={select.onChange}>
+                        {select.values.map(v => <option key={v.id} value={v.value}>{v.value}</option>)}
+                    </Form.Control>
             </Form.Group>
             <p></p>
             <div><Button variant="info" type="submit">Add</Button></div>
