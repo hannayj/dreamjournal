6a258f85c44e26150231877a2d63cad0ab45fe45
Hanna Ylä-Jussila <hanna.yla-jussila@myy.haaga-helia.fi>
2019-11-17 15:28:47 +0200

    datetimepicker not working

--- frontend/src/components/AddSleepPeriodForm.js
+++ frontend/src/components/AddSleepPeriodForm.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import DateFnsUtils from '@date-io/date-fns';
+import MuiPickersUtilsProvider from '@material-ui/pickers';
+import DateTimePicker from '@material-ui/pickers';
 import Button from 'react-bootstrap/Button';
 
 const AddSleepPeriodForm = ({
@@ -6,7 +9,7 @@ const AddSleepPeriodForm = ({
   setStartTime,
   endTime,
   setEndTime,
-  addSleepPeriod,
+  addSleepPeriod
 }) => {
   return (
     <div id='new'>
@@ -16,23 +19,29 @@ const AddSleepPeriodForm = ({
       <form onSubmit={ addSleepPeriod() }>
         <label htmlFor='startTime'>Start time</label>
         <br />
-        <input
-          onChange={event => setStartTime(event.target.value)}
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <DateTimePicker
+          //onChange={event => setStartTime(event.target.value)}
+          onChange={setStartTime}
           type='datetime-local'
           id='startTime'
           name='startTime'
           value={ startTime }
         />
+        </MuiPickersUtilsProvider>
         <br />
         <label htmlFor='endTime'>End time</label>
         <br />
-        <input
-          onChange={event => setEndTime(event.target.value)}
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+         <DateTimePicker
+          //onChange={event => setEndTime(event.target.value)}
+          onChange={setEndTime}
           type='datetime-local'
           id='endTime'
           name='endTime'
           value={ endTime }
         />
+        </MuiPickersUtilsProvider>
         <br />
         <p></p>
         <Button variant='info' type='submit'>Add new sleep period</Button>
